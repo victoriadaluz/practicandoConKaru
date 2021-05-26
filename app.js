28e734ffcba40c31eebe30f86dacb6236ee68541
victoriadaluz <vicky.daluz@gmail.com>
2021-05-26 19:25:49 -0300

    aca practicando

---
+++ app.js
@@ -0,0 +1,94 @@
+let alumnxs = [ // Base de datos
+    {
+        nombre: "Jonatan",
+        apellido: "Céspedes",
+        materias: [
+            {
+                nombre: "Sociales",
+                estado: "Aprobado"
+            },
+            {
+                nombre: "Naturales",
+                estado: "Aprobado"
+            },{
+                nombre: "Matemáticas",
+                estado: "Desaprobado"
+            },{
+                nombre: "Inglés",
+                estado: "Aprobado"
+            },{
+                nombre: "Lengua",
+                estado: "Aprobado"
+            },
+        ]
+    },
+    {
+        nombre: "Matias",
+        apellido: "López",
+        materias: [
+            {
+                nombre: "Sociales",
+                estado: "Aprobado"
+            },
+            {
+                nombre: "Naturales",
+                estado: "Aprobado"
+            },{
+                nombre: "Matemáticas",
+                estado: "Desaprobado"
+            },{
+                nombre: "Inglés",
+                estado: "Aprobado"
+            },{
+                nombre: "Lengua",
+                estado: "Aprobado"
+            },
+        ]
+    },
+    {
+        nombre: "María",
+        apellido: "Cordoba",
+        materias: [
+            {
+                nombre: "Sociales",
+                estado: "Aprobado"
+            },
+            {
+                nombre: "Naturales",
+                estado: "Aprobado"
+            },{
+                nombre: "Matemáticas",
+                estado: "Desaprobado"
+            },{
+                nombre: "Inglés",
+                estado: "Aprobado"
+            },{
+                nombre: "Lengua",
+                estado: "Aprobado"
+            },
+        ]
+    },
+    {
+        nombre: "Luis",
+        apellido: "Muñoz",
+        materias: [
+            {
+                nombre: "Sociales",
+                estado: "Aprobado"
+            },
+            {
+                nombre: "Naturales",
+                estado: "Aprobado"
+            },{
+                nombre: "Matemáticas",
+                estado: "Desaprobado"
+            },{
+                nombre: "Inglés",
+                estado: "Aprobado"
+            },{
+                nombre: "Lengua",
+                estado: "Aprobado"
+            },
+        ]
+    },
+]
